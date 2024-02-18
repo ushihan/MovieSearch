c60abf8cd1113e8c8124426346a24eb01a265211
Shih-Han Hsu <zerowaft@gmail.com>
2024-02-18 14:04:10 +1000

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Shih-Han Hsu on 18/2/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
